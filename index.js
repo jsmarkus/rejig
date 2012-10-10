30a8c910b8f7cb3b3fe4065590842adf9c504271
Mark <nixmrak@yandex.ru>
2012-10-10 10:32:02 +0300

    init js

---
+++ index.js
@@ -0,0 +1,34 @@
+var Parser = require("jison").Parser;
+
+var grammar = {
+    lex: {
+        rules: [
+            ["[^{}\\[\\]]+", "return 'WORD'"],
+            ["\\{", "return '{'"],
+            ["\\}", "return '}'"],
+            ["\\[", "return '['"],
+            ["\\]", "return ']'"],
+            ["$", "return 'EOF'"],
+            [".", "return 'INVALID'"]
+        ]
+    },
+    start: "expressions",
+    bnf: {
+        "expressions": [
+            ["e EOF", " return $1; "]
+        ],
+        "e": [
+            ["e e", " $$ = $1 + $2; "],
+            ["[ e ]", " $$ = Math.random()>0.5 ? $2 : ''; "],
+            ["{ e }", " var arr = String($2).split('|'); $$ = arr[parseInt(Math.random() * arr.length)];"],
+            ["WORD", " $$ = String(yytext); "]
+        ]
+    }
+};
+
+
+var parser = new Parser(grammar);
+
+module.exports = function (text) {
+    return parser.parse(text);
+};
